adfd70f72aff4dcc74149a8ee3449fd26ee1e904
Stoyan <stoyanmanolovvv@gmail.com>
2022-08-29 17:37:31 +0300

    Clipboard test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 
 function App() {
+  const toBase64 = async (url) => {
+    try {
+      const dataUrl = await toDataURL(url);
+      if (typeof dataUrl !== "string") return;
+      return dataUrl.replace(/(data:[^]*;base64,)/g, "");
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+
+  const makeImagePromise = async (base64) => {
+    const response = await fetch(
+    );
+    return await response.blob(base64);
+  };
+
   const handleClick = () => {
-    const makeImagePromise = async () => {
-      const response = await fetch(
-        "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
-      );
-      return await response.blob();
-    };
+    const base64url = toBase64("https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png")
 
     navigator.clipboard
-      .write([new ClipboardItem({ "image/png": makeImagePromise() })])
+      .write([new ClipboardItem({ "image/png": makeImagePromise(base64url) })])
       .then(function () {
         console.log("copied");
       })
